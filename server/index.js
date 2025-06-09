cc372c68ad5eb6dbfbaf559085c7d819bb298b1e
Ubuntu <DevopsTestingVM@DevopsTestingVM.gmphr30sumcefebiyoide5nh4g.zx.internal.cloudapp.net>
2025-06-09 08:39:08 +0000

    Application deployed in git

---
+++ server/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+app.get("/", (_req, res) => {
+  res.send("Backend is running and connected to MongoDB.");
+});
+
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
+app.get("/test-db", async (_req, res) => {
+  try {
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    return res.json({ collections });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
